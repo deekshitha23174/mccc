165f40e42d707f18faff866e0ae7d450c82ced74
deekshitha <22h51a05f8@cmrcet.ac.in>
2024-11-24 15:39:06 +0530

    first commit

---
+++ code.java
@@ -0,0 +1,7 @@
+// code.java - A simple Java program that prints "Hello, World!"
+
+public class Code {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
